78b92afe0913d786893b06e8ba027d880f2dd21b
Igor Augusto <76701857+IIgorAugustoI@users.noreply.github.com>
2024-08-14 16:38:04 +0000

    Rodando o prettier para formatação do texto.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-function Home(){
-    return (
-        <h1>Apesar de estar bravo, eu te amo muito!</h1>
-        
-    )
-
+function Home() {
+  return <h1>Apesar de estar bravo, eu te amo muito!</h1>;
 }
 
 export default Home;
