b341cf5e076d73bcbb9a327d38549319d83f3050
Igor Augusto <76701857+IIgorAugustoI@users.noreply.github.com>
2024-12-10 19:28:55 +0000

    Atualizando secrets dos arquivos.

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,12 @@
 import { Client } from "pg";
-import "dotenv/config";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
