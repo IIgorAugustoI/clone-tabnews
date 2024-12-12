3ce09b03ea2fbb65d206d2a8b422fa36587ef83c
Igor Augusto <76701857+IIgorAugustoI@users.noreply.github.com>
2024-12-12 19:27:44 +0000

    Finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 2 as sum;");
-  console.log(result.rows);
-  res.status(200).json("Alunos do curso.dev são demais!");
+  // Update time
+  const updatedAt = new Date().toISOString();
+
+  // Query version POSTGRES - Expected 16.6
+  const queryPgVersionResult = await database.query("SHOW server_version;");
+  const versionPgValue = queryPgVersionResult.rows[0].server_version;
+
+  // Get the max number off supported connections
+  const queryMaxConnectionResult = await database.query(
+    "SHOW MAX_CONNECTIONS;",
+  );
+  const maxConnectionValue = parseInt(
+    queryMaxConnectionResult.rows[0].max_connections,
+  );
+
+  // Get the number of active users
+  const databaseName = process.env.POSTGRES_DB;
+  const queryPgUserActivesResult = await database.query({
+    text: "select count(*)::int from pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const userActivesValue = queryPgUserActivesResult.rows[0].count;
+
+  // Response
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version_pg: versionPgValue,
+        max_connections: maxConnectionValue,
+        user_connected: userActivesValue,
+      },
+    },
+  });
 }
 
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  // console.log(responseBody);
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  // Checks version POSTGRES
+  expect(responseBody.dependecies.database.version_pg).toEqual("16.6");
+
+  // Checks the number of user connected
+  expect(responseBody.dependecies.database.user_connected).toEqual(1);
+
+  // Checks the max number of user connect supported
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
 });
